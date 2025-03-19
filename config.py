306e5d21c3ccd3200b7046ae6c1b6b684da7cd2d
Farhan Ahmad <farhanhubble@gmail.com>
2025-03-20 00:54:42 +0800

    FEAT: Config module

---
+++ config.py
@@ -0,0 +1,15 @@
+import json
+from pydantic import BaseModel, RootModel, Field
+from pathlib import Path
+
+
+class Config(BaseModel):
+    pass
+
+
+with open(".config/default.json", "r") as f:
+    config = Config(**json.load(f))
+
+
+if __name__  == "__main__":
+    print(config.model_dump_json(indent=2))
