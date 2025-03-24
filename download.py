f4e0d9b8b7e7dff01bffd7674283153eac9e199c
Farhan Ahmad <farhanhubble@gmail.com>
2025-03-25 02:52:16 +0800

    FEAT: Enable link scraping from metadata

--- download.py
+++ download.py
@@ -1,16 +1,15 @@
 import os
+import pandas as pd
 import requests
+from _logging import logger
 from bs4 import BeautifulSoup
+from config import config
 from tqdm import tqdm
 from urllib.parse import urljoin
 
-def download_archive(page_url, download_folder="pdf_downloads"):
-    print(f"Downloading PDFs from {page_url} to {download_folder}")
-    # Create download folder if it doesn't exist
-    if not os.path.exists(download_folder):
-        os.makedirs(download_folder)
 
-    # Fetch the page
+def _pdf_list_from_page(page_url):
+    logger.info(f"Extracting PDF links from download page: {page_url}")
     response = requests.get(page_url)
     response.raise_for_status()  # Raise an error if the request failed
 
@@ -30,6 +29,34 @@ def download_archive(page_url, download_folder="pdf_downloads"):
 
     # Remove duplicates if any
     pdf_links = list(set(pdf_links))
+    return pdf_links
+
+
+def _pdf_list_from_metadata(metadata_url):
+    logger.info(f"Extracting PDF links from metadata at: {metadata_url}")
+    response = requests.get(metadata_url)
+    response.raise_for_status() 
+
+    # Load the excel file
+    df = pd.read_excel(metadata_url)
+    pdf_links = df["File Name"].tolist()
+    return pdf_links
+
+def download_archive(metadata_url, page_url, download_folder="pdf_downloads"):
+    print(f"Downloading PDFs from {page_url} to {download_folder}")
+    # Create download folder if it doesn't exist
+    if not os.path.exists(download_folder):
+        os.makedirs(download_folder)
+
+    # Fetch the page
+    response = requests.get(page_url)
+    response.raise_for_status()  # Raise an error if the request failed
+
+    # Get all PDF links
+    if metadata_url:
+        pdf_links = _pdf_list_from_metadata(metadata_url)
+    else:
+        pdf_links = _pdf_list_from_page(page_url)
 
     # Download each PDF
     for pdf_url in tqdm(pdf_links, desc="Downloading PDFs"):
@@ -50,13 +77,9 @@ def download_archive(page_url, download_folder="pdf_downloads"):
             print(f"Failed to download {pdf_url}: {e}")
 
 if __name__ == "__main__":
-    url = "https://www.archives.gov/research/jfk/release-2017-2018"
-    download_archive(url, "./data/archives.gov/2017-2018")
-    url = "https://www.archives.gov/research/jfk/release-2021"
-    download_archive(url, "./data/archives.gov/2021")
-    url = "https://www.archives.gov/research/jfk/release-2022"
-    download_archive(url, "./data/archives.gov/2022")
-    url = "https://www.archives.gov/research/jfk/release-2023"
-    download_archive(url, "./data/archives.gov/2023")
-    url = "https://www.archives.gov/research/jfk/release-2025"
-    download_archive(url, "./data/archives.gov/2025")
+    sources = config.download.URLS
+    download_dir = config.download.download_dir
+    for year in sources:
+        metadata_url = sources[year]["metadata_url"]
+        page_url = sources[year]["data_url"]
+        download_archive(metadata_url, page_url, download_dir / year)
