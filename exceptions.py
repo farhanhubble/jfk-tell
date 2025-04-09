f98dabec32dad201b6f341984ad4fc6457d5236c
Farhan Ahmad <farhanhubble@gmail.com>
2025-04-09 22:39:19 +0800

    FEAT: Windowed exception rate calculation

--- exceptions.py
+++ exceptions.py
@@ -1,4 +1,6 @@
 """Exception monitoring and handling tools"""
+
+from collections import deque
 from typing import Callable, List
 from datetime import datetime
 import functools
@@ -6,23 +8,36 @@ import time
 import traceback
 import os
 
+
 class ExceptionMonitor:
     """Monitor the number of exceptions raised and the number of calls made to a function."""
 
-    def __init__(self, error_rate_threshold: float, min_calls: int):
+    def __init__(
+        self, error_rate_threshold: float, min_calls: int, window_sz: int = 100
+    ):
         """
         :param error_rate_threshold: The error rate threshold in [0,1), above which an exception is raised.
         :param min_calls: The minimum number of calls before the error rate is calculated.
         """
         self.error_rate_threshold = error_rate_threshold
         self.min_calls = min_calls
-        self.nb_exceptions = 0
-        self.nb_calls = 0
-        self.error_rate = 0
         self.exceptions_log = "exceptions.log"
         if os.path.exists(self.exceptions_log):
             os.remove(self.exceptions_log)
 
+        if window_sz < min_calls:
+            raise ValueError(
+                f"Window size {window_sz} must be greater than or equal to min_calls {min_calls}."
+            )
+        self.window_sz = window_sz
+        self.recent_outcomes = deque(maxlen=window_sz)
+
+    def _error_rate_exceeded(self):
+        if len(self.recent_outcomes) < self.min_calls:
+            return False
+        return (
+            sum(self.recent_outcomes) / len(self.recent_outcomes)
+        ) > self.error_rate_threshold
 
     def _save_exception(self):
         with open("exceptions.log", "a") as f:
@@ -33,15 +48,14 @@ class ExceptionMonitor:
     def __call__(self, func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            self.nb_calls += 1
             try:
-                return func(*args, **kwargs)
+                result = func(*args, **kwargs)
+                self.recent_outcomes.append(False)
+                return result
             except Exception:
-                self.nb_exceptions += 1
-                if self.nb_calls >= self.min_calls:
-                    self.error_rate = self.nb_exceptions / self.nb_calls
-                    if self.error_rate > self.error_rate_threshold:
-                        raise
+                self.recent_outcomes.append(True)
+                if self._error_rate_exceeded():
+                    raise
                 self._save_exception()
 
         return wrapper
